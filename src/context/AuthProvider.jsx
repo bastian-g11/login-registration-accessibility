7993433474cd4125d8839616492706d672e00147
Sebastian Gomez Ramirez <Sebastian.Gomez-Ramirez@endava.com>
2023-02-27 15:54:53 -0500

    FEAT: Add useMemo hook in auth provider

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,15 +1,25 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useMemo } from 'react';
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
 
-  return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
-      {children}
-    </AuthContext.Provider>
+  /** If your component renders the same result given the same props,
+   * you can wrap it in a call to React.memo for a performance boost
+   * in some cases by memoizing the result.
+   * This means that React will skip rendering the component,
+   * and reuse the last rendered result. */
+
+  const value = useMemo(
+    () => ({
+      auth,
+      setAuth,
+    }),
+    [auth]
   );
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export default AuthContext;
